ef3d47810d39de1dd9d5123b5a48de108c1e1c6a
Waris Ahmed <warisahmed2022@gmail.com>
2025-09-02 15:06:37 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import HomeOnePage from "./(homes)/home-1/page";
 
 export const metadata: Metadata = {
-  title: "Liko - Home Page",
+  title: "Nexus Solutions",
 };
 
 export default function Home() {
