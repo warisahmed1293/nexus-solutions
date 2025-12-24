56c8909d15ba5de59b7d670cfc63c619a889657d
Waris Ahmed <warisahmed2022@gmail.com>
2025-12-24 18:37:07 +0500

    PHASE-4

--- src/app/service-details/[slug]/page.tsx
+++ src/app/service-details/[slug]/page.tsx
@@ -1,29 +1,36 @@
-// app/service-details/[slug]/page.tsx
 "use client";
-import { gsap } from "gsap";
+
 import React, { useEffect } from "react";
+import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import { useParams, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
+
 import useScrollSmooth from "@/hooks/use-scroll-smooth";
 import { ScrollSmoother, ScrollTrigger, SplitText } from "@/plugins";
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
-// internal imports
+// layouts & components
 import Wrapper from "@/layouts/wrapper";
 import HeaderEleven from "@/layouts/headers/header-eleven";
 import ServiceDetailsArea from "@/components/service/service-details-area";
 import LineImgSlider from "@/components/line-text/line-img-slider";
 import BigText from "@/components/big-text";
 import FooterTwo from "@/layouts/footers/footer-two";
+
+// data & utils
 import { getServiceBySlug } from "@/data/service-details-data";
-// animation
 import { charAnimation, titleAnimation } from "@/utils/title-animation";
 
-const ServiceDetailsPage = () => {
-  const params = useParams();
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function ServiceDetailsPage({ params }: PageProps) {
   const router = useRouter();
-  const slug = params.slug as string;
-  
+  const slug = params.slug;
+
   useScrollSmooth();
 
   useGSAP(() => {
@@ -31,53 +38,35 @@ const ServiceDetailsPage = () => {
       charAnimation();
       titleAnimation();
     }, 100);
+
     return () => clearTimeout(timer);
   });
 
-  // Get service data by slug
   const service = getServiceBySlug(slug);
 
-  // Redirect to 404 if service not found
   useEffect(() => {
     if (!service) {
-      router.push('/error');
+      router.push("/error");
     }
   }, [service, router]);
 
-  // Show nothing while redirecting
-  if (!service) {
-    return null;
-  }
+  if (!service) return null;
 
   return (
     <Wrapper>
-      {/* header area start */}
       <HeaderEleven />
-      {/* header area end */}
 
       <div id="smooth-wrapper">
         <div id="smooth-content">
           <main>
-            {/* service details area */}
             <ServiceDetailsArea service={service} />
-            {/* service details area */}
-
-            {/* line image slider  */}
             <LineImgSlider />
-            {/* line image slider  */}
-
-            {/* big text */}
             <BigText />
-            {/* big text */}
           </main>
 
-          {/* footer area */}
           <FooterTwo topCls="" />
-          {/* footer area */}
         </div>
       </div>
     </Wrapper>
   );
-};
-
-export default ServiceDetailsPage;
+}
